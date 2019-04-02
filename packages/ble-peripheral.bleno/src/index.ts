780e149cf7e5864be36700cd0dd98b44c56765a8
Carter Harrison <carterjharrison@gmail.com>
2019-04-01 17:59:15 -0700

    export bleno types

--- packages/ble-peripheral.bleno/src/index.ts
+++ packages/ble-peripheral.bleno/src/index.ts
@@ -1,1 +1,5 @@
 
+export { BlenoCharacteristic } from './bleno-characteristic'
+export { BlenoDescriptor } from './bleno-descriptor'
+export { BlenoServer } from './bleno-server'
+export { BlenoService } from './bleno-service'
