1c81092a54c44c5eacf96ab73171941a44f55d05
Carter Harrison <carterjharrison@gmail.com>
2019-04-03 17:27:50 -0700

    introduce delay

--- packages/network.ble/src/xyo-full-ble-network.ts
+++ packages/network.ble/src/xyo-full-ble-network.ts
@@ -2,6 +2,7 @@ import { IXyoNetworkProvider, IXyoNetworkProcedureCatalogue, IXyoNetworkPipe } f
 import { XyoClientBluetoothNetwork } from './xyo-client-ble-network'
 import { XyoServerNetwork } from './xyo-server-ble-network'
 import { XyoLogger } from '@xyo-network/logger';
+import { XyoBase } from '@xyo-network/base';
 
 
 export class XyoFullBleNetwork implements IXyoNetworkProvider {
@@ -31,23 +32,41 @@ export class XyoFullBleNetwork implements IXyoNetworkProvider {
 
       if (pipeFromServer) {
         found = true
+        this.clientHandle(false)
+        this.serverHandle(false)
         return pipeFromServer
       }
 
       this.serverHandle(false)
       this.clientHandle(true)
 
+      await this.delay(5000)
+
       const pipeFromClient = await this.client.findWithTimeout(30_000)
 
       if (pipeFromClient) {
         found = true
+        this.clientHandle(false)
+        this.serverHandle(false)
         return pipeFromClient
       }
 
       this.clientHandle(false)
+
+      await this.delay(5000)
     }
 
     throw new Error("Invalid state")
+  }
+
+  private delay (mills: number): Promise<void> {
+    return new Promise((resolve, reject) => {
+      const onDone = () => {
+        resolve()
+      }
+
+      XyoBase.timeout(onDone, mills)
+    })
   }
 
   public async stopServer(): Promise <void> {
