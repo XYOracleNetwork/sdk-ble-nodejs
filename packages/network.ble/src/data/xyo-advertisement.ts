ca3722caf44fd9030610b07924bb8ad82f1ee7c6
Carter Harrison <carterjharrison@gmail.com>
2019-04-05 16:48:42 -0700

    write uints not ints to advertisement

--- packages/network.ble/src/data/xyo-advertisement.ts
+++ packages/network.ble/src/data/xyo-advertisement.ts
@@ -12,8 +12,8 @@ export class XyoAdvertisement {
         const firstBuffer = Buffer.from("02011A1AFF4C000215D684352EDF36484EBC982D5398C5593E", "hex")
         const secondBuffer = Buffer.alloc(5)
 
-        secondBuffer.writeInt16BE(this.major, 0)
-        secondBuffer.writeInt16BE(this.minor, 2)
+        secondBuffer.writeUInt16BE(this.major, 0)
+        secondBuffer.writeUInt16BE(this.minor, 2)
 
         // todo get rssi at 1m instead of fixed -50 
         secondBuffer.writeInt8(-50, 4)
@@ -23,8 +23,6 @@ export class XyoAdvertisement {
 
 
     public getScanResponse (): Buffer {
-
-        // todo shave off major and minor
         return Buffer.from(
             "11073E59C598532D98BC4E4836DF2E3584D6",
             "hex"
