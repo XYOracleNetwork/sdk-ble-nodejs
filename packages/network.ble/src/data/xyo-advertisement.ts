14809a16db694b36cc0e93f0856e169884bfb0dd
Carter Harrison <carterjharrison@gmail.com>
2019-04-09 14:32:18 -0700

    major/minor in uuid

--- packages/network.ble/src/data/xyo-advertisement.ts
+++ packages/network.ble/src/data/xyo-advertisement.ts
@@ -18,13 +18,25 @@ export class XyoAdvertisement {
         // todo get rssi at 1m instead of fixed -50 
         secondBuffer.writeInt8(-50, 4)
 
+
         return Buffer.concat([firstBuffer, secondBuffer])
     }
 
+    
 
+    // "11073E59C598532D98BC4E4836DF 2E 35 84 D6"
     public getScanResponse (): Buffer {
+        let uuid = "11073E59C598532D98BC4E4836DF"
+        const majorBuff = Buffer.alloc(2)
+        majorBuff.writeInt16BE(this.major, 0)
+        const minorBuff = Buffer.alloc(2)
+        majorBuff.writeInt16BE(this.major, 0)
+
+        uuid = uuid + majorBuff.toString("hex")[1] + majorBuff.toString("hex")[0]
+        uuid = uuid + minorBuff.toString("hex")[1] + minorBuff.toString("hex")[0]
+
         return Buffer.from(
-            "11073E59C598532D98BC4E4836DF2E3584D6",
+            uuid.toUpperCase(),
             "hex"
         ) 
     }
