6d8b5ce26fac9130d31399f48da0a93ce0208ded
Carter Harrison <carterjharrison@gmail.com>
2020-04-06 10:56:58 -0700

    rebase

--- packages/ble-peripheral.bleno/src/index.ts
+++
@@ -1,5 +0,0 @@
-
-export { BlenoCharacteristic } from './bleno-characteristic'
-export { BlenoDescriptor } from './bleno-descriptor'
-export { BlenoServer } from './bleno-server'
-export { BlenoService } from './bleno-service'
--- packages/network.ble/src/index.ts
+++
@@ -1,3 +0,0 @@
-export { XyoClientBluetoothNetwork } from './xyo-client-ble-network'
-export { XyoServerNetwork } from './xyo-server-ble-network'
-export { XyoFullBleNetwork } from './xyo-full-ble-network'
