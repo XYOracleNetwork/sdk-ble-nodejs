69a76e6dd7fa7e49d6881d687c94bbad6d99b732
Carter Harrison <carterjharrison@gmail.com>
2020-04-06 11:47:46 -0700

    central plugin

--- src/central/noble/index.ts
+++ src/central/noble/index.ts
@@ -1,1 +1,26 @@
-export { NobleScan } from './noble-scan'
+import { IXyoPlugin, IXyoPluginDelegate } from '@xyo-network/sdk-base-nodejs'
+import { NobleScan } from './noble-scan'
+
+export class XyoBleNoble implements IXyoPlugin {
+  public BLE_CENTRAL: any | undefined
+
+  public getName(): string {
+    return 'ble-noble'
+  }
+
+  public getProvides(): string[] {
+    return ['BLE_CENTRAL']
+  }
+
+  public getPluginDependencies(): string[] {
+    return []
+  }
+
+  public async initialize(delegate: IXyoPluginDelegate): Promise<boolean> {
+    const scanner = new NobleScan()
+
+    this.BLE_CENTRAL = scanner
+
+    return true
+  }
+}
