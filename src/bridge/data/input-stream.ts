8c2083eea80f67bd6ffafb9511ffae98f8c25ca7
Carter Harrison <carterjharrison@gmail.com>
2020-06-01 21:32:29 -0700

    fix: data from gitignore

---
+++ src/bridge/data/input-stream.ts
@@ -0,0 +1,52 @@
+export class XyoInputStream {
+  private donePackets: Buffer[] = []
+  private nextWaitingSize: number | undefined
+  private currentBuffer: Buffer | undefined
+
+  public addChunk(chunk: Buffer): void {
+    const currentBuffer = this.currentBuffer
+    const nextWaitingSize = this.nextWaitingSize
+
+    if (currentBuffer && nextWaitingSize) {
+      this.currentBuffer = Buffer.concat([currentBuffer, chunk])
+    } else {
+      this.currentBuffer = chunk
+    }
+
+    this.checkSize()
+    this.checkDone()
+  }
+
+  public getOldestPacket(): Buffer | undefined {
+    if (this.donePackets.length > 0) {
+      const oldestPacket = this.donePackets[0]
+      this.nextWaitingSize = undefined
+      this.currentBuffer = undefined
+      return oldestPacket
+    }
+
+    return undefined
+  }
+
+  private checkSize(): void {
+    const currentBuffer = this.currentBuffer
+
+    if (currentBuffer) {
+      if (currentBuffer.length >= 4) {
+        this.nextWaitingSize = currentBuffer.readUInt32BE(0)
+      }
+    }
+  }
+
+  private checkDone(): void {
+    const waitingSize = this.nextWaitingSize
+    const currentBuffer = this.currentBuffer
+
+    if (waitingSize && currentBuffer) {
+      if (waitingSize <= currentBuffer.length) {
+        const donePacket = currentBuffer.slice(4, this.nextWaitingSize)
+        this.donePackets.push(donePacket)
+      }
+    }
+  }
+}
